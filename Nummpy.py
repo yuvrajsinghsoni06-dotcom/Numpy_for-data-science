fa9b28f7ba08a270966fa758d6008e7216a3d5b3
yuvrajsonigreat <yuvrajsinghsoni06@gmail.com>
2026-06-28 17:23:42 +0530

    Refactor Nummpy.py to comment out unused code and improve readability

--- Nummpy.py
+++ Nummpy.py
@@ -2,14 +2,28 @@ import numpy as np
 
 
 
-print(np.__version__)
+# print(np.__version__)
 
 # my_list = [1,2,3,4]
 # my_list = my_list * 2
 
 # print(my_list)
-arr = np.array([1,2,3,4,5])
-arr = arr * 2
-print(arr)
+# arr = np.array([1,2,3,4,5])
+# arr = arr * 2
+# print(arr)
 
-print(type(arr))
+# print(type(arr))
+
+
+array = np.array([[["A","b","c"], ["d","e","f"], ["i","j","k"]],
+                  [["k","l","m"], ["n","o","p"], ["q","r","s"]],
+                  [["t","u","v"], ["w","x","y"], ["z","o","g"]]])
+# print(array.ndim)  # ndim - attribute calcluates no of dimension of arr
+# print(array.shape) # shape - attribute It calculates the number of rows, columns, and number of layers in an array. 
+
+
+# chain indexing - A multi-dimensional indexing (arr[layer_no,row_no,column_no])
+print(array[1,1,0])
+
+master =array[2,1,2] +  array[2,0,1] + array[2,0,2] + array[1,2,1] + array[0,0,0] + array[0,2,1]
+print(master)
