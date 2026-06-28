d7553f7aceb508025ee7c9779d3ff89899057973
yuvrajsonigreat <yuvrajsinghsoni06@gmail.com>
2026-06-28 17:02:15 +0530

    Add initial Nummpy.py file with basic numpy operations

---
+++ Nummpy.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+
+print(np.__version__)
+
+# my_list = [1,2,3,4]
+# my_list = my_list * 2
+
+# print(my_list)
+arr = np.array([1,2,3,4,5])
+arr = arr * 2
+print(arr)
+
+print(type(arr))
