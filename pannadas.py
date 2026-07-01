2d9aa0d410f661e2280325046d4be7d6bd538dc6
yuvrajsonigreat <yuvrajsinghsoni06@gmail.com>
2026-07-01 19:52:45 +0530

    Enhance pandas series examples in pannadas.py by adding detailed comments and implementing new DataFrame rows with concatenation.

--- pannadas.py
+++ pannadas.py
@@ -8,7 +8,7 @@ data = [1,2,3,4,5]
 series = pd.Series(data, index = ["a","b","c","d","e"])
 # print(series)
 
-# loc = property which allows to accces the elements of a series by its lable(index) 
+# loc = property which allows to accces the elements of a series by its lable(index) (location by label) ccan also be used in dataframe
 # series.loc["a"] = 1000
 
 # print(series)
@@ -19,10 +19,36 @@ series = pd.Series(data, index = ["a","b","c","d","e"])
 
 # print(series[series > 2.5])  # i can apply filter in a series of data
 
-calories = {"day1": 2230, "day2": 2500, "day3" : 2000}
-series = pd.Series(calories)
+# calories = {"day1": 2230, "day2": 2500, "day3" : 2000}
+# series = pd.Series(calories)
 
-series.loc["day1"] = 2800
-# print(series)
+# series.loc["day1"] = 2800
+# # print(series)
 
-print(series[series <= 2500])
+# print(series[series <= 2500])
+
+
+
+# dataframe - It's a two-dimensional label data structure with columns and rows, similar to a spreadsheet or a SQL table. 
+
+data = {"Name": ["morphes", "john smith", "neo", "trinity"],
+        "Id" : [1,2,3,4] ,
+        "Salary" : [10000,20000,30000,10000]}
+
+dataframe = pd.DataFrame(data, index = ["employee1", "employee2","employee3","employee4"])
+# print(dataframe.loc["employee3"])
+
+
+# print(dataframe.iloc[3])
+
+dataframe["Role"] = ["Cammander", "Agent" , "the Chosen One", "the baddie"]   #  Addition of new column in df
+
+# addition of new row in df - so we forst create a seperate dataframe and then concat it into exixting dataframe
+df_row = pd.DataFrame({"Name": "matrix", "Id" : 5, "Salary" : 50000,
+                       "Role" : "the world of illusion"}, index = ["employee5"])
+df_row1 = pd.DataFrame({"Name": "sexy red dress women", "Id" : 6, "Salary" : 50000,
+                       "Role" : "the hot latina"}, index = ["employee6"])
+df_row2 = pd.DataFrame({"Name": "rick", "Id" : 5, "Salary" : 50000,
+                       "Role" : "the crazy piece of shit"}, index = ["employee7"])
+dataframe = pd.concat([dataframe, df_row, df_row1, df_row2])
+print(dataframe)
